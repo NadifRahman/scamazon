ea02a6b4be86f2aa8e8a8fb66efde16da0af8191
NadifRahman <nadif.rah@gmail.com>
2024-05-31 17:12:36 -0400

    Create home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,12 @@
+import logo from '../../assets/scamazon-logo.png';
+import HomeStyle from './Home.module.css';
 function Home() {
-  return 'hi';
+  return (
+    <div className={HomeStyle.container}>
+      <div className={HomeStyle.welcome}>Welcome to</div>
+      <img className={HomeStyle.img} src={logo} alt="Company logo" />
+    </div>
+  );
 }
 
 export default Home;
